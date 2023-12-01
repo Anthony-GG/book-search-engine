bf9ec8e491c9389ca692b9cedca7d16325bd6c13
Anthony Iacano <iacano5@outlook.com>
2023-11-30 19:14:15 -0500

    added apollo logic to app page, need to fix routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,17 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'; //imports the Apollo logic into the app
 
 import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <ApolloProvider client={client}>
+      <>
+        <Navbar />
+        <Outlet />
+      </>
+    </ApolloProvider>
   );
 }
 
