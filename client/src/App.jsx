9ba433aeef341d2c099d561c8985a8327ea5efc2
Anthony Iacano <iacano5@outlook.com>
2023-11-30 22:46:37 -0500

    fixed issue with auth that Ross went over

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,30 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'; //imports the Apollo logic into the app
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client'; //imports the Apollo logic into the app
 
 import Navbar from './components/Navbar';
 
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
 //instance of the ApolloClient creation
 const client = new ApolloClient({
-  uri: 'http://localhost:3001/graphql', 
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
