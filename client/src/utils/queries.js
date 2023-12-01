9ec8e09b2aa4b6f2a577a045633285bc14090f35
Anthony Iacano <iacano5@outlook.com>
2023-11-30 19:07:36 -0500

    added the queries and mutations js files to interact with the written backend

---
+++ client/src/utils/queries.js
@@ -0,0 +1,21 @@
+import { gql } from '@apollo/client';
+
+//this is a query that gets the current info of the current user
+export const GET_USER = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
